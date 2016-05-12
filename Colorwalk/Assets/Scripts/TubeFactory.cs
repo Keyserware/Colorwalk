e55b42afbb90ff7e341a45b91d7cb2851eea3277
Hans Rauer <h.w.rauer@googlemail.com>
2016-05-13 00:10:21 +0200

    finished createRandPath

--- Colorwalk/Assets/Scripts/TubeFactory.cs
+++ Colorwalk/Assets/Scripts/TubeFactory.cs
@@ -6,18 +6,21 @@ public class TubeFactory : MonoBehaviour {
 	public float offsetAngle;
 	public float offsetDist;
 
-	private static IList<Vector3> createRandPath(Vector3 start, Vector3 dir, int count, float offsetAngle, float offsetDist) {
+	private static IList<Vector3> createRandPath(Vector3 start, Vector3 dir, int count, float offsetRadius, float offsetDist) {
 		IList<Vector3> list = new List<Vector3>();
 		list.Add(start);
+
 		Vector3 prevPoint = start;
 		dir = Vector3.Normalize(dir);
-		float theta = Mathf.Acos(dir.z);
-		float phi = Mathf.Atan2(dir.y, dir.x);
+
 		for(int c = 0; c < count; c++) {
-			theta += Random.Range(-1.0f, 1.0f) * offsetAngle;
-			phi += Random.Range(-1.0f, 1.0f) * offsetAngle;
-			Vector3 nextPoint = prevPoint;
-			
+			Vector3 help = new Vector3(dir.x, -dir.y, -dir.z);
+			Vector3 normal = Vector3.Normalize(Vector3.Cross(dir, help));
+			Vector3 nextPoint = Quaternion.AngleAxis(Random.Range(0.0f, 360.0f), dir) * (prevPoint + dir * offsetDist + normal * offsetRadius);
+			list.Add(nextPoint);
+
+			dir = Vector3.Normalize(nextPoint - prevPoint);
+			prevPoint = nextPoint;
 		}
 
 		return list;
